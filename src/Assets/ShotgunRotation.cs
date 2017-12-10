3753f4b1c22ffe75317365069c7cc11438a1b943
Artem Vasylenko <artemjjke@gmail.com>
2017-12-10 22:30:02 +0200

    fixed shotgun rotation if character is facing left

--- src/Assets/ShotgunRotation.cs
+++ src/Assets/ShotgunRotation.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets._2D;
 
 public class ShotgunRotation : MonoBehaviour
 {
-    public int maxRotation = 45;
+    public int maxRotation = 55;
+    private MainCharacterMovementControl _mainCharacterMovement;
+
+    void Awake()
+    {
+        _mainCharacterMovement = transform.GetComponentInParent<MainCharacterMovementControl>();
+        if (_mainCharacterMovement == null)
+        {
+            Debug.LogError("Character movement is missing");
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
@@ -15,8 +25,15 @@ public class ShotgunRotation : MonoBehaviour
     {
         if (Input.GetMouseButton(1))
         {
-            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 difference = mousePos - transform.position;
             difference.Normalize();
+
+            if (!_mainCharacterMovement.facingRight)
+            {
+                difference *= -1;
+            }
+
             float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
             if (rotationZ <= -maxRotation) // down
